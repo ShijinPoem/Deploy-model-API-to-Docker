75e767ce7d85d1b203b3bf295523f269ca97eb77
Poem179 <www.toshibadt@gmail.com>
2026-03-19 09:28:59 +0700

    the baseline

---
+++ app.py
@@ -0,0 +1,22 @@
+from fastapi import FastAPI
+import joblib
+import numpy as np
+
+app = FastAPI()
+
+# Load model khi start server
+model = joblib.load("iris_model.pkl")
+
+@app.get("/")
+def home():
+    return {"message": "Iris model API is running"}
+
+@app.post("/predict")
+
+def predict(data: dict):
+    features = np.array(data["features"]).reshape(1, -1)
+    prediction = model.predict(features)
+
+    return {
+        "prediction": int(prediction[0])
+    }
